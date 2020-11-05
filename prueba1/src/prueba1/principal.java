71626b7cd9a8732832d9cdfb316a61250132bea7
bt <bt@PC-2.mshome.net>
2020-11-05 21:27:50 +0100

    fase tres

--- prueba1/src/prueba1/principal.java
+++ prueba1/src/prueba1/principal.java
@@ -4,6 +4,10 @@ public class principal {
 
 	public static void main(String[] args) {
 		System.out.println("hola mundo java-git ");
+		System.out.println("hola mundo java-git2 ");
+		System.out.println("hola mundo java-git3 ");
+		System.out.println("hola mundo java-git4 ");
+		
 	
 	
 	}
